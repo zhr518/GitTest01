e3b56649f720ef0a3a9727f1e8b1997e2a2a1f85
zhanghaoran <2014086284@qq.com>
2021-01-07 22:56:53 +0800

    第一次提交测试

---
+++ src/com/zhang/Hello.java
@@ -0,0 +1,13 @@
+package com.zhang;
+
+import java.util.Arrays;
+
+/**
+ * @author zhanghaoran
+ * @create 2021-01-07 22:55
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("args = Hello World!" );
+    }
+}
